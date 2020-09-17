a00eefae9c67e6e4465aa6dd5c589dc2e3674348
Loke <lokeshwaran.k@aricent.com>
2020-09-18 03:24:00 +0530

    Assignment Project Setup

---
+++ src/main/java/com/astrazeneca/azassignment/AzAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.astrazeneca.azassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astrazeneca/azassignment/AzAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astrazeneca.azassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
